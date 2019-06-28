7ceef1aa38a0b60e2ade65c3b3af6e34670bad91
VanSickle-Wendi <gwendells@gmail.com>
2019-06-27 20:23:37 -0600

    Working on Project 2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const connectionString = process.env.DATABASE_URL || 'postgress://bvzlovwlmvybyc:428e6809e72e8ad2fee3eb1f4be3a3e6ba0c89903fa60e4b6ba8da6746bcdb3a@ec2-107-21-216-112.compute-1.amazonaws.com:5432/d93jk4ojlqvufu?ssl=true';
+const connectionString = process.env.DATABASE_URL || 'postgres://yzenztejzuqtcn:45b7db1c822f4f51a191cf9516b4c7a5fdf5bcaa2fc0b357bcce3d6d8bb0c078@ec2-107-21-216-112.compute-1.amazonaws.com:5432/d9lqqa5vmtg0r2?ssl=true';
 
 const pool = new Pool({connectionString: connectionString});
 
